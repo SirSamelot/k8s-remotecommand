fd609ab453f1ad18396a718acbd926f2dd00e71c
samwong <samhwwong@gmail.com>
2018-05-31 14:08:57 -0500

    Working remote command with args

--- rc.go
+++ rc.go
@@ -8,9 +8,12 @@ import (
 	"path/filepath"
 	"strings"
 
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
 )
 
 type Writer struct {
@@ -41,6 +44,7 @@ func homeDir() string {
 func main() {
 	fmt.Println("/////////////// Remote Command PoC ///////////////")
 
+	// get the kubeconfig
 	var kubeconfig *string
 	if home := homeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -56,39 +60,45 @@ func main() {
 	namespace := "default"
 	podName := "bb"
 
-	// Testing access to API
+	// Testing that we are talking to kubernetes
 	pods, _ := kubeClient.CoreV1().Pods(namespace).List(metav1.ListOptions{})
 	fmt.Printf("There are %d pods in the namespace '%s'\n", len(pods.Items), namespace)
 
 	pod, _ := kubeClient.CoreV1().Pods(namespace).Get(podName, metav1.GetOptions{})
-	fmt.Println(pod)
-	// HERE up to line 44 of searchlight/lib.go
+	containerName := pod.Spec.Containers[0].Name
+	fmt.Printf("Container Name: %s\n", containerName)
 
-	// execRequest := kubeClient.CoreV1().RESTClient().Post().
-	// 	Resource("pods").
-	// 	Name("bb").
-	// 	Namespace("default").
-	// 	SubResource("exec").
-	// 	Param("container", "bb").
-	// 	Param("command", "ls").
-	// 	Param("stdin", "true").
-	// 	Param("stdout", "true").
-	// 	Param("stderr", "false").
-	// 	Param("tty", "false")
+	// create the request
+	execRequest := kubeClient.CoreV1().RESTClient().Post()
+	execRequest = execRequest.
+		Resource("pods").
+		Name(podName).
+		Namespace(namespace).
+		SubResource("exec")
+	execRequest.VersionedParams(&v1.PodExecOptions{
+		Container: containerName,
+		Command:   []string{"ls", "-al"},
+		Stdout:    true,
+		Stderr:    true,
+		Stdin:     true,
+	}, scheme.ParameterCodec)
 
-	// // Here we go
-	// exec, _ := remotecommand.NewSPDYExecutor(config, "POST", execRequest.URL())
+	fmt.Printf("URL:\t%s\n", execRequest.URL())
+	// Create the executor
+	exec, _ := remotecommand.NewSPDYExecutor(config, "POST", execRequest.URL())
 
-	// stdIn := newStringReader([]string{"-c", ""})
-	// stdOut := new(Writer)
-	// stdErr := new(Writer)
+	stdIn := newStringReader([]string{"-c", "-al"})
+	stdOut := new(Writer)
+	stdErr := new(Writer)
 
-	// _ = exec.Stream(remotecommand.StreamOptions{
-	// 	Stdin:  stdIn,
-	// 	Stdout: stdOut,
-	// 	Stderr: stdErr,
-	// 	Tty:    false,
-	// })
+	// Stream the command
+	_ = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  stdIn,
+		Stdout: stdOut,
+		Stderr: stdErr,
+		Tty:    false,
+	})
+	fmt.Printf("stdIn:\t%s\nstdOut:\t%s\nstdErr:\t%s\n", stdIn, stdOut, stdErr)
 
-	// fmt.Println(stdOut)
+	fmt.Println("----------- FINISHED -------------")
 }
